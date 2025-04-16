f090c2dfdf06eb25a98d1fdbfd14fc1c3fbcb055
JaafarAlMuallim <Ja3far03@gmail.com>
2025-04-17 00:46:11 +0300

    deploy gg

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { Chart } from "@/components/chart";
+import MaxWidthWrapper from "@/components/max-width-wrapper";
+
+const Page = () => {
+  return (
+    <MaxWidthWrapper>
+      <Chart />
+    </MaxWidthWrapper>
+  );
+};
+
+export default Page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
+import MaxWidthWrapper from "@/components/max-width-wrapper";
+import ViewChirps from "@/components/view-chirps";
+import ChirpForm from "@/components/chirp-form";
+
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-      </div>
-    </main>
+    <MaxWidthWrapper className="flex flex-col gap-4">
+      <ChirpForm />
+      <ViewChirps />
+    </MaxWidthWrapper>
   );
 }
