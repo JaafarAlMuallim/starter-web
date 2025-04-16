3612a16551b2cbbda8b0d07babaf7d1b0b69bef4
JaafarAlMuallim <Ja3far03@gmail.com>
2025-04-17 01:11:10 +0300

    prod

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,21 +1,6 @@
 import { env } from "@/env";
 import { createAuthClient } from "better-auth/react"; // make sure to import from better-auth/react
-import { type NextRequest, NextResponse } from "next/server";
 
 export const authClient = createAuthClient({
   baseURL: env.NEXT_PUBLIC_BETTER_AUTH_URL,
 });
-
-export async function authMiddleware(req: NextRequest) {
-  const { data: session } = await authClient.getSession({
-    fetchOptions: {
-      headers: {
-        cookie: req.headers.get("cookie") || "",
-      },
-    },
-  });
-  if (!session) {
-    return NextResponse.redirect(new URL("/sign-in", req.url));
-  }
-  return NextResponse.next();
-}
